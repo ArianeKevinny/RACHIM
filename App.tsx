f2da10c5ffecf6add5cb77045a87fd1a017c02bd
Ariane Kevinny <51418537+ArianeKevinny@users.noreply.github.com>
2023-04-30 18:49:54 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import {View} from 'react-native';
-import {Routes} from './src/routes';
+/* eslint-disable prettier/prettier */
+import React, { useEffect, useState } from 'react';
+import auth from '@react-native-firebase/auth';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthRoutes from './src/routes'
+import MyTabs from './src/routes/myTabs'
+
 
 function App(): JSX.Element {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <NavigationContainer>
+        <AuthRoutes />
+      </NavigationContainer>
+    );
+  }
+
   return (
-    <View>
-      <Routes />
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
 
